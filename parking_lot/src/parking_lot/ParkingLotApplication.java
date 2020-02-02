5bf57f0dc6f2067067c019907da8802a3e9fce18
jain-somil <jain.somil@gmail.com>
2020-02-02 18:16:34 +0530

    File Mode

--- parking_lot/src/parking_lot/ParkingLotApplication.java
+++ parking_lot/src/parking_lot/ParkingLotApplication.java
@@ -1,38 +1,66 @@
 package parking_lot;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 import parking_lot.operations.OperationsController;
 
 public class ParkingLotApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		
 		Object[] parkingLot = null;
 		
-		System.out.println("Starting the Program in Interactive Mode..");
-		System.out.println("Please enter the commands: ");
-		Scanner sc = new Scanner(System.in);
 		String inputLine;
-		while(true) {
-			inputLine = sc.nextLine();
-			String[] commands = inputLine.split(" ");
-			switch(commands.length) {
-			case 1: parkingLot = OperationsController.performOperation(parkingLot, commands[0]);
-					break;
-			case 2: parkingLot = OperationsController.performOperation(parkingLot, commands[0], commands[1]);
-					break;
-			case 3: parkingLot = OperationsController.performOperation(parkingLot, commands[0], commands[1], commands[2]);
-					break;
-			default: System.out.println("Invalid Input");
-					break;
-			
+		
+		if(args.length != 0) {
+			File file = new File(args[0]);
+			System.out.println("Starting the Program in File Mode..");
+			try (Scanner sc = new Scanner(file)){
+				while(sc.hasNextLine()) {
+					inputLine = sc.nextLine();
+					parkingLot = issueCommands(inputLine, parkingLot);
+					if(parkingLot == null)
+						break;
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-			if(parkingLot == null)
-				break;
+			
+		}else {
+			System.out.println("Starting the Program in Interactive Mode..");
+			System.out.println("Please enter the commands: ");
+			try(Scanner sc = new Scanner(System.in)){
+				while(true) {
+					inputLine = sc.nextLine();
+					parkingLot = issueCommands(inputLine, parkingLot);
+					if(parkingLot == null)
+						break;
+				}
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
-		sc.close();
-
+	}
+	
+	private static Object[] issueCommands(String input, Object[] parkingLot) {
+		String[] commands = input.split(" ");
+		switch(commands.length) {
+		case 1: parkingLot = OperationsController.performOperation(parkingLot, commands[0]);
+				break;
+		case 2: parkingLot = OperationsController.performOperation(parkingLot, commands[0], commands[1]);
+				break;
+		case 3: parkingLot = OperationsController.performOperation(parkingLot, commands[0], commands[1], commands[2]);
+				break;
+		default: System.out.println("Invalid Input");
+				break;
+		
+		}
+		
+		return parkingLot;
 	}
 
 }
